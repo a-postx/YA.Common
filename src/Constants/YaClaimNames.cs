f9b5ad3597b03a6d16672efcc0aefefd197a6819
Алексей Якубин <2412719@mail.ru>
2020-11-09 13:19:30 +0300

    Добавлены имена удостоверений и публикация пакетов, в.1.0.6

--- src/Constants/YaClaimNames.cs
+++ src/Constants/YaClaimNames.cs
@@ -7,9 +7,11 @@
         public const string sub = nameof(sub);
         public const string tid = nameof(tid);
         public const string email = nameof(email);
+        public const string emailVerified = nameof(emailVerified);
         public const string username = nameof(username);
         public const string name = nameof(name);
         public const string role = nameof(role);
         public const string externalId = nameof(externalId);
+        public const string azp = nameof(azp);
     }
 }
