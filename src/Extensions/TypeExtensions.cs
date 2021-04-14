8a0896c821b17fe9d1edd1f44028df2c9ced127b
Алексей Якубин <2412719@mail.ru>
2021-04-14 18:58:37 +0300

    Добавлены расширения типов

---
+++ src/Extensions/TypeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace YA.Common.Extensions
+{
+    public static class TypeExtensions
+    {
+        ///<summary>Возвращает признак того, что тип объекта содержит свойство.</summary>
+        public static bool HasProperty(this Type obj, string propertyName)
+        {
+            return obj.GetProperty(propertyName) != null;
+        }
+    }
+}
