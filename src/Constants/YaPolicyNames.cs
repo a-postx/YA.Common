a035751a1b9b0c6601303b78b2424b6fc89f0edd
Алексей Якубин <2412719@mail.ru>
2021-06-22 17:18:52 +0300

    Добавлены имена политик доступа

---
+++ src/Constants/YaPolicyNames.cs
@@ -0,0 +1,10 @@
+﻿namespace YA.Common.Constants
+{
+    public static class YaPolicyNames
+    {
+        public const string Owner = nameof(Owner);
+        public const string Admin = nameof(Admin);
+        public const string Writer = nameof(Writer);
+        public const string Reader = nameof(Reader);
+    }
+}
